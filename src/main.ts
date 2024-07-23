8ea9439faeea51b4843d1ab668c6778cc77b1917
ThIsJaCk <y23657689@gmail.com>
2024-07-23 23:01:06 +0800

    Init

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import Main from '@/Main.vue'
+import router from './router'
+
+const app = createApp(Main)
+
+app.use(router)
+
+app.mount('#app')
