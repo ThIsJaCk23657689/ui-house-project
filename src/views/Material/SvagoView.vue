ece7911eddae2b0fac180e0251d99646ed351b04
ThIsJaCk <y23657689@gmail.com>
2024-08-23 05:11:20 +0800

    更新 Svago 洗碗機

--- src/views/Material/SvagoView.vue
+++ src/views/Material/SvagoView.vue
@@ -4,12 +4,16 @@ import Page01View from './Svago/Page01.vue'
 import Page02View from './Svago/Page02.vue'
 import Page03View from './Svago/Page03.vue'
 import Page04View from './Svago/Page04.vue'
+import Page05View from './Svago/Page05.vue'
+import Page06View from './Svago/Page06.vue'
 
 const pages = [
     { content: Page01View, isDark: true },
     { content: Page02View, isDark: true },
     { content: Page03View, isDark: true },
     { content: Page04View, isDark: true },
+    { content: Page05View, isDark: true },
+    { content: Page06View, isDark: true },
 ];
 
 const buttons = [
