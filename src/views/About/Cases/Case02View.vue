0e114b41957a1f996fb62e150b49ea892500ac6e
ThIsJaCk <y23657689@gmail.com>
2024-09-25 07:23:23 +0800

    澄品 完成

--- src/views/About/Cases/Case02View.vue
+++ src/views/About/Cases/Case02View.vue
@@ -1,9 +1,24 @@
 <script setup lang="ts">
+import CaseWrapper from '@/layouts/CaseWrapper.vue';
+import Page01View from './Case02/Page01.vue';
+import Page02View from './Case02/Page02.vue';
+import Page03View from './Case02/Page03.vue';
+import Page04View from './Case02/Page04.vue';
+import Page05View from './Case02/Page05.vue';
+
+const pages = [
+    { content: Page01View },
+    { content: Page02View },
+    { content: Page03View },
+    { content: Page04View },
+    { content: Page05View },
+];
 
 </script>
 
 <template>
-02
+<CaseWrapper :pages="pages">
+</CaseWrapper>
 </template>
 
 <style scoped>
